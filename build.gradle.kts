c75a00ddc7e1f85efc8ed67869a5b0ed1d58a1eb
cotrin1208 <gummy8672@gmail.com>
2024-04-06 13:29:42 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
+val ktorVersion: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.22"
+    id("com.vanniktech.maven.publish") version "0.28.0"
+}
+
+group = "io.github.cotrin8672"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+afterEvaluate {
+    mavenPublishing {
+        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = false)
+        signAllPublications()
+
+        coordinates(
+            group.toString(),
+            name,
+            version.toString()
+        )
+
+        pom {
+            name.set("Ktor Line Webhook Signature Validation Plugin")
+            url.set("https://github.com/cotrin8672/ktor-line-webhook-plugin")
+            description.set("Ktor plugin for validating signature with line messaging api webhook")
+            licenses {
+                license {
+                    name.set("MIT License")
+                    url.set("https://github.com/cotrin8672/ktor-line-webhook-plugin/LICENSE")
+                    distribution.set("repo")
+                }
+            }
+            developers {
+                developer {
+                    id.set("cotrin8672")
+                    name.set("Cotrin")
+                    email.set("gummy8672@gmail.com")
+                }
+            }
+            scm {
+                url.set("https://github.com/cotrin8672/ktor-line-webhook-plugin")
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/LineWebhookSignatureValidation.kt
@@ -0,0 +1,38 @@
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import java.util.*
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+
+val LineWebhook = createRouteScopedPlugin(
+    name = "LineWebhookSignatureValidationPlugin",
+    createConfiguration = ::Configuration
+) {
+    suspend fun ApplicationCall.verifySignature(channelSecret: String): Boolean {
+        val signatureFromHeader = request.header(LineSignature) ?: return false
+        val key = SecretKeySpec(channelSecret.toByteArray(), "HmacSHA256")
+        val mac = Mac.getInstance("HmacSHA256").apply {
+            init(key)
+        }
+        val source = receiveText().toByteArray(Charsets.UTF_8)
+        val calculatedSignature = Base64.getEncoder().encodeToString(mac.doFinal(source))
+        return signatureFromHeader == calculatedSignature
+    }
+
+    pluginConfig.apply {
+        onCall { call ->
+            if (!call.verifySignature(channelSecret)) {
+                call.respond(HttpStatusCode.Forbidden)
+                return@onCall
+            }
+        }
+    }
+}
+
+class Configuration {
+    lateinit var channelSecret: String
+}
+
+const val LineSignature = "X-Line-Signature"
